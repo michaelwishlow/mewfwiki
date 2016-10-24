c41645d22c31ecf64c6634ab9b432c857a722eb4
michaelwishlow <michael@pantheritconsulting.com>
2016-10-24 10:19:23 -0700

    Add Categories to Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,12 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @articles = Article.all.order("created_at DESC") 
+    if params[:category].blank?
+       @articles = Article.all.order("created_at DESC")
+    else
+       @category_id = Category.find_by(name: params[:category]).id
+       @articles = Article.where(category_id: @category_id).order("created_at DESC")
+    end 
   end
 
   def show
